d730cf6f37be4746a2b1779ac1b4985e8829ef95
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-08-25 15:35:02 +0330

    calculation of total price added for any order

--- app/models.py
+++ app/models.py
@@ -38,7 +38,7 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
+from django.db.models import Sum
+from django.http import Http404, JsonResponse
+
+from app.models import Order
+
+
 def checkout(request, order_pk):
-    pass
+    try:
+        order = Order.objects.get(pk=order_pk)
+    except Order.DoesNotExist:
+        raise Http404
+
+    total_price = Sum(item.product.price * item.quantity for item in order.items.all)
+    return JsonResponse({'total_price', total_price})
+
+
 
