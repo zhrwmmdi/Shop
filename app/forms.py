4f9a9fdf09afe2fb5d7fd33ae0d6a3bed97eb0f2
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-08-25 15:22:30 +0330

    product form with validation constraints added

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,24 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 from .models import Product
 
 
+def price_validator(value):
+    if value > 1000:
+        raise ValidationError('Product is too expensive')
+
+
+def description_validator(value):
+    if len(value) < 20:
+        raise ValidationError('Product must have a good description')
+
+
 class ProductForm(ModelForm):
-    pass
+
+    price = forms.DecimalField(max_digits=10, decimal_places=2, validators=[price_validator,])
+    description = forms.TextInput(blank=True, validators=[description_validator, ])
+
+    class Meta:
+        model = Product
+        fields = ['name', 'category', 'description', 'price', 'stock']
